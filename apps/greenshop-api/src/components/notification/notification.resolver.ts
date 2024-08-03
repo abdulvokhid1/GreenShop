c3c57a3e81fbcff57805f3fa48bf7cb4caf5e6fa
ali <ibrakhimov7747a@gmail.com>
2024-08-03 13:55:17 +0900

    feat: greenshop project backend has been finished

--- apps/greenshop-api/src/components/notification/notification.resolver.ts
+++ apps/greenshop-api/src/components/notification/notification.resolver.ts
@@ -2,12 +2,7 @@ import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
 import { NotificationService } from './notification.service';
 // import NotificationSchema from '../../schemas/Notification.model';
 import { Notification, Notifications } from '../../libs/dto/notification/notification';
-import { Roles } from '../auth/decorators/roles.decorator';
-import { AuthMember } from '../auth/decorators/authMember.decorator';
-import { UseGuards } from '@nestjs/common';
-import { ObjectId } from 'mongoose';
 import { shapeIntoMongoObjectId } from '../../libs/config';
-import { WithoutGuard } from '../auth/guards/without.guard';
 import { NotificationInput } from '../../libs/dto/notification/notification.input';
 @Resolver()
 export class NotificationResolver {
